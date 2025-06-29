87ead849172650a9211870d45b30a2f88ea6e7a3
steniomoreira <steniomoreiradev@gmail.com>
2025-06-29 10:45:44 -0300

    fix: update refine confirmation password

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -28,33 +28,35 @@ import {
 import { Input } from '@/components/ui/input'
 import { api } from '@/lib/axios'
 
-const signupSchema = z.object({
-  firstName: z.string().trim().min(1, {
-    message: 'O nome é obrigatório.',
-  }),
-  lastName: z.string().trim().min(1, {
-    message: 'O sobrenome é obrigatório.',
-  }),
-  email: z
-    .string()
-    .email({ message: 'O e-mail é inválido.' })
-    .trim()
-    .min(1, { message: 'O e-mail é obrigatório.' }),
-  password: z
-    .string()
-    .trim()
-    .min(6, { message: 'A senha deve ter no mínino 6 caracteres.' }),
-  passwordConfirmation: z
-    .string()
-    .trim()
-    .min(6, { message: 'A confirmação de senha é obrigatória.' })
-    .refine((data) => data.passwordConfirmation === data.password, {
-      message: 'As senhas devem ser iguais.',
+const signupSchema = z
+  .object({
+    firstName: z.string().trim().min(1, {
+      message: 'O nome é obrigatório.',
     }),
-  terms: z.boolean().refine((value) => value === true, {
-    message: 'Você precisa aceitar os termos.',
-  }),
-})
+    lastName: z.string().trim().min(1, {
+      message: 'O sobrenome é obrigatório.',
+    }),
+    email: z
+      .string()
+      .email({ message: 'O e-mail é inválido.' })
+      .trim()
+      .min(1, { message: 'O e-mail é obrigatório.' }),
+    password: z
+      .string()
+      .trim()
+      .min(6, { message: 'A senha deve ter no mínino 6 caracteres.' }),
+    passwordConfirmation: z
+      .string()
+      .trim()
+      .min(6, { message: 'A confirmação de senha é obrigatória.' }),
+    terms: z.boolean().refine((value) => value === true, {
+      message: 'Você precisa aceitar os termos.',
+    }),
+  })
+  .refine((data) => data.passwordConfirmation === data.password, {
+    message: 'As senhas devem ser iguais.',
+    path: ['passwordConfirmation'],
+  })
 
 const SignUpPage = () => {
   const [user, setUser] = useState(null)
