1abc4c3821f396009df57deae3f101c4318c18e2
steniomoreira <steniomoreiradev@gmail.com>
2025-06-29 22:19:03 -0300

    feat: add api integration on login page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,6 +1,9 @@
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation } from '@tanstack/react-query'
+import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import PasswordInput from '@/components/password-input'
@@ -22,6 +25,7 @@ import {
   FormMessage,
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
+import { api } from '@/lib/axios'
 
 const loginSchema = z.object({
   email: z
@@ -36,6 +40,7 @@ const loginSchema = z.object({
 })
 
 const LoginPage = () => {
+  const [user, setUser] = useState(null)
   const form = useForm({
     resolver: zodResolver(loginSchema),
     defaultValues: {
@@ -44,8 +49,70 @@ const LoginPage = () => {
     },
   })
 
+  const loginMutation = useMutation({
+    mutationKey: ['login'],
+    mutationFn: async (variables) => {
+      const response = await api.post('/users/login', {
+        email: variables.email,
+        password: variables.password,
+      })
+
+      return response.data
+    },
+  })
+
   const handleSubmit = (data) => {
-    console.log(data)
+    localStorage.getItem('accessToken')
+    localStorage.getItem('refreshToken')
+
+    loginMutation.mutate(data, {
+      onSuccess: (loggedUser) => {
+        const accessToken = loggedUser.tokens.accessToken
+        const refreshToken = loggedUser.tokens.refreshToken
+
+        localStorage.setItem('accessToken', accessToken)
+        localStorage.setItem('refreshToken', refreshToken)
+
+        setUser(loggedUser)
+
+        toast.success('Login realizado com sucesso!')
+      },
+      onError: () => {
+        toast.error(
+          'Erro ao logar na conta. Por favor, tente novamente mais tarde.'
+        )
+      },
+    })
+  }
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const accessToken = localStorage.getItem('accessToken')
+        const refreshToken = localStorage.getItem('refreshToken')
+
+        if (!accessToken && !refreshToken) return
+
+        const response = await api.get('/users/me', {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        })
+
+        setUser(response.data)
+      } catch (error) {
+        console.error(error)
+
+        localStorage.removeItem('accessToken')
+        localStorage.removeItem('refreshToken')
+      }
+    }
+
+    init()
+  }, [])
+
+  if (user) {
+    return <h1>Olá, {user.first_name}</h1>
   }
 
   return (
