8a43890e1bbf88e409fc00fccae19d9a66400918
steniomoreira <steniomoreiradev@gmail.com>
2025-07-07 00:30:23 -0300

    feat: add protected and public api instances

--- src/context/auth.jsx
+++ src/context/auth.jsx
@@ -6,7 +6,7 @@ import {
   LOCAL_STORAGE_ACCESS_TOKEN_KEY,
   LOCAL_STORAGE_REFRESH_TOKEN_KEY,
 } from '@/constants/local-storage'
-import { api } from '@/lib/axios'
+import { protectedApi, publicApi } from '@/lib/axios'
 
 export const AuthContext = createContext({
   user: null,
@@ -35,7 +35,7 @@ export const AuthContextProvider = ({ children }) => {
   const signupMutation = useMutation({
     mutationKey: ['signup'],
     mutationFn: async (variables) => {
-      const response = await api.post('/users', {
+      const response = await publicApi.post('/users', {
         first_name: variables.firstName,
         last_name: variables.lastName,
         email: variables.email,
@@ -49,7 +49,7 @@ export const AuthContextProvider = ({ children }) => {
   const loginMutation = useMutation({
     mutationKey: ['login'],
     mutationFn: async (variables) => {
-      const response = await api.post('/users/login', {
+      const response = await publicApi.post('/users/login', {
         email: variables.email,
         password: variables.password,
       })
@@ -107,7 +107,7 @@ export const AuthContextProvider = ({ children }) => {
 
         if (!accessToken && !refreshToken) return
 
-        const response = await api.get('/users/me')
+        const response = await protectedApi.get('/users/me')
 
         setUser(response.data)
       } catch (error) {
--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,11 +2,15 @@ import axios from 'axios'
 
 import { LOCAL_STORAGE_ACCESS_TOKEN_KEY } from '@/constants/local-storage'
 
-export const api = axios.create({
+export const publicApi = axios.create({
   baseURL: 'https://fullstackclub-finance-dashboard-api.onrender.com/api',
 })
 
-api.interceptors.request.use((request) => {
+export const protectedApi = axios.create({
+  baseURL: 'https://fullstackclub-finance-dashboard-api.onrender.com/api',
+})
+
+protectedApi.interceptors.request.use((request) => {
   const accessToken = localStorage.getItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY)
 
   if (!accessToken) {
