b2e7a354a64af8198dc1beb32f3ab50506a92eb9
steniomoreira <steniomoreiradev@gmail.com>
2025-07-22 13:01:27 -0300

    fix: ensure from and to query params are only considered if they are valid

--- src/components/date-selection.jsx
+++ src/components/date-selection.jsx
@@ -1,5 +1,5 @@
 import { useQueryClient } from '@tanstack/react-query'
-import { addMonths, format } from 'date-fns'
+import { addMonths, format, isValid } from 'date-fns'
 import { useEffect, useState } from 'react'
 import { useNavigate, useSearchParams } from 'react-router'
 
@@ -9,20 +9,38 @@ import { DatePickerWithRange } from './ui/date-picker-with-range'
 
 const formatDateToQueryParam = (date) => format(date, 'yyy-MM-dd')
 
+const getInitialDate = (searchParams) => {
+  const defaultDate = {
+    from: new Date(),
+    to: addMonths(new Date(), 1),
+  }
+
+  const from = searchParams.get('from')
+  const to = searchParams.get('to')
+
+  if (!from || !to) {
+    return defaultDate
+  }
+
+  const dateAreInvalid = !isValid(new Date(from)) || !isValid(new Date(to))
+
+  if (dateAreInvalid) {
+    return defaultDate
+  }
+
+  return {
+    from: new Date(from + 'T00:00:00'),
+    to: new Date(to + 'T00:00:00'),
+  }
+}
+
 const DateSelction = () => {
   const navigate = useNavigate()
   const [searchParams] = useSearchParams()
   const queryClient = useQueryClient()
   const { user } = useAuthContext()
 
-  const [date, setDate] = useState({
-    from: searchParams.get('from')
-      ? new Date(searchParams.get('from') + 'T00:00:00')
-      : new Date(),
-    to: searchParams.get('to')
-      ? new Date(searchParams.get('to') + 'T00:00:00')
-      : addMonths(new Date(), 1),
-  })
+  const [date, setDate] = useState(getInitialDate(searchParams))
 
   useEffect(() => {
     if (!date?.from || !date?.to) return
